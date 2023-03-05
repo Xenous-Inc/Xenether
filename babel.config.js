0d1ed943b3321846760308c0137a0af2b46dea2f
ARTHEDA <arth3da@gmail.com>
2023-03-05 16:02:55 +0300

    Configuration

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,23 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
+module.exports = function (api) {
+    api.cache(true);
+    return {
+        presets: ['babel-preset-expo'],
+        plugins: [
+            [
+                'module-resolver',
+                {
+                    root: ['./'],
+                    alias: {
+                        '@assets': './src/assets',
+                        '@components': './src/components',
+                        '@styles': './src/styles',
+                        '@navigation': './src/navigation',
+                        '@screens': './src/screens',
+                        '@utils': './src/utils',
+                    },
+                },
+            ],
+            'react-native-reanimated/plugin',
+        ],
+    };
 };
