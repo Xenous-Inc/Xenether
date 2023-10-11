3c146e83fa86c39e2d630921182b7af58218912b
dmitsai <dmitiisai456@gmail.com>
2023-10-11 18:33:21 +0600

    [Feature] add screen with offer to add cities

--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
-import { StyleSheet, TouchableOpacity, Image } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, TouchableOpacity, Image, View, Text } from 'react-native';
 import Colors from '@styles/colors';
 import PagerView from 'react-native-pager-view';
 import { CurrentScreen } from '@screens/CurrentScreen';
 import * as Location from 'expo-location';
+import { PermissionStatus } from 'expo-location';
 import { Screens, Stacks } from '@navigation/constants';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { TAppStackParams } from '@navigation/AppNavigator';
@@ -15,6 +16,8 @@ import { PlaceSkeleton } from '@components/PlaceSkeleton';
 import { Status } from '@storage/types';
 import { createGetWeatherAction } from '../store/slices/weatherSlice';
 import { createGetCityAction } from '../store/slices/citySlice';
+import { useTheme } from '../model/themeContext';
+import { Theme } from '@storage/constants';
 
 const apiKey = 'f467ce1b7a6266168a069f38c99d7029';
 
@@ -23,14 +26,18 @@ export const MainScreen: React.FC<
         NativeStackScreenProps<TMainStackParams, typeof Screens.Main.MAIN>,
         NativeStackScreenProps<TAppStackParams>
     >
-> = () => {
+> = props => {
+    const theme = useTheme();
+    const { navigation } = props;
+    const [status, requestPermission] = Location.useForegroundPermissions();
     const [currentIndex, setCurrentIndex] = useState(0);
-    const { data: cities, status } = useAppSelector(state => state.cities);
+    const { data: cities, status: city_status } = useAppSelector(state => state.cities);
     type TCoords = {
         long: number;
         lat: number;
     };
     const dispatch = useAppDispatch();
+
     const getCurrentLocation = async () => {
         try {
             await Location.requestForegroundPermissionsAsync();
@@ -56,15 +63,43 @@ export const MainScreen: React.FC<
 
     const getDataByLocation = async () => {
         const coords: TCoords | null = await getCurrentLocation();
-        const data = await getWeather(coords);
-        dispatch(createGetWeatherAction(data.name));
-        dispatch(createGetCityAction(data.name));
+        if (coords) {
+            const data = await getWeather(coords);
+            dispatch(createGetWeatherAction(data.name));
+            dispatch(createGetCityAction(data.name));
+        }
+        return undefined;
     };
 
-    if (status === Status.Idle) {
+    if (city_status === Status.Idle && status?.status === PermissionStatus.GRANTED) {
         getDataByLocation();
     }
-
+    if (status?.status !== PermissionStatus.GRANTED || cities?.length === 0) {
+        return (
+            <View style={[styles.empty_cities_wrapper, { backgroundColor: theme.colors.accentColor }]}>
+                <Image
+                    source={
+                        theme.themeMode === Theme.DARK
+                            ? require('@assets/icons/white-sun.png')
+                            : require('@assets/icons/icon-sun.png')
+                    }
+                    style={styles.empty_cities_icon}
+                />
+                <Text style={[styles.empty_cities_message, { color: theme.colors.textcolor }]}>
+                    Пока что тут нет городов
+                </Text>
+                <Text style={[styles.empty_cities_comment, { color: theme.colors.textcolor }]}>
+                    Вы можете добавить город в{' '}
+                    <Text
+                        style={styles.empty_cities_button}
+                        onPress={() => navigation.navigate(Stacks.SETTINGS, { screen: Screens.Settings.MAIN })}
+                    >
+                        настройках
+                    </Text>
+                </Text>
+            </View>
+        );
+    }
     return (
         <>
             <PagerView
@@ -85,19 +120,28 @@ export const MainScreen: React.FC<
 };
 
 const styles = StyleSheet.create({
-    containerButton: {
+    empty_cities_wrapper: {
+        flex: 1,
         alignItems: 'center',
-        backgroundColor: Colors.LIGHT_WHITE,
-        width: 52,
-        height: 52,
-        borderRadius: 15,
-        position: 'absolute',
-        zIndex: 1,
-        right: 20,
+        justifyContent: 'center',
     },
-    iconSettings: {
-        marginTop: 14,
-        width: 22,
-        height: 22,
+    empty_cities_message: {
+        fontFamily: 'ExpandedBold',
+        fontSize: 14,
+        marginBottom: 10,
+    },
+    empty_cities_comment: {
+        fontFamily: 'ExpandedSemiBold',
+        fontSize: 12,
+    },
+    empty_cities_button: {
+        color: Colors.LIGHT_ORANGE,
+        fontFamily: 'ExpandedBold',
+        fontSize: 12,
+    },
+    empty_cities_icon: {
+        marginBottom: 20,
+        height: 40,
+        width: 36,
     },
 });
